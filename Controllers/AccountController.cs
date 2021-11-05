3ebfc573042543662d441fe3601d61d149fb9c38
Vfed <asser.282195@gmail.com>
2021-11-05 18:02:26 +0200

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using APITest.Data;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace APITest.Controllers
+{
+    public class AccountController : Controller
+    {
+        // тестовые данные вместо использования базы данных
+        private List<Person> people = new List<Person>
+        {
+            new Person {Login="admin@gmail.com", Password="12345", Role = "admin" },
+            new Person { Login="qwerty@gmail.com", Password="55555", Role = "user" }
+        };
+
+        [HttpPost("/token")]
+        public IActionResult Token([FromBody]PersonDto dto)
+        {
+
+
+            var identity = GetIdentity(dto.Login, dto.Password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            //создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            return Json(response);
+        }
+
+        [Authorize]
+        [HttpGet("/getuserrole")]
+        public IActionResult GetRole()
+        {
+            return Ok("Test is complite");
+        }
+
+        [HttpGet("/getuserclaims")]
+        public List<Claim> GetRole2()
+        {
+            var claims = User.Claims.ToList();
+            return claims;
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            Person person = people.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
+        }
+    }
+}
